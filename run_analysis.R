5abc6caa84a4bae1a9ec6cbd39ec9dfc80e6f8c1
guillaumedreyer <dreyfactor@ygmail.com>
2015-01-25 15:57:35 -0800

    This is a third update.

--- run_analysis.R
+++ run_analysis.R
@@ -1,36 +1,92 @@
-# Set the working directory. 
-setwd("~/Dropbox/Coursera_Data_Sci/Part3/Project")
+
+# 0. Preliminaries: Get the data and store them in 
+# the working directory.
+
+# Download the data to the working directory. 
+
+library(downloader)
+
+UCI_link <- 'https://d396qusza40orc.cloudfront.net/
+getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip' 
+
+working_directory <- getwd()
+
+
+download(url = UCI_link,
+         dest = paste(working_directory,
+                      '/UCI_Samsung_Galaxy_S_data.zip',
+                      sep= ''),
+         mode = "wb") 
 
 
 
-# Question 1: Get and merge the datasets. 
 
-# Unzip the the zip file and store the content in "Data_Project" directory.
-unzip('~/Dropbox/Coursera_Data_Sci/Part3/Project/getdata_projectfiles_UCI HAR Dataset.zip',
+#download.file(url = UCI_link,
+#destfile = paste(working_directory, 
+ #                              '/UCI_Samsung_Galaxy_S_data.zip',
+  #                             sep = ''),
+   #           method = 'curl',
+    #          mode = 'wb')
+
+#temp <- tempfile()
+
+#download.file(UCI_link,
+ #             temp, 
+  #            method = 'curl',
+   #           mode = 'wb')
+
+
+path_to_zip_file <- paste(working_directory, 
+                          '/UCI_Samsung_Galaxy_S_data.zip',
+                          sep = '')
+
+unzip(path_to_zip_file,
       overwrite = T,
-      exdir = "Data_Project")
+      exdir = "Samsung_Data")
 
 
-# Extract the training and test 'X' and 'Y' datasets and merge them together.
-X_train <- read.table('./Data_Project/UCI HAR Dataset/train/X_train.txt', quote= '')
-X_test <- read.table('./Data_Project/UCI HAR Dataset/test/X_test.txt', quote= '')
+
+# Unzip the zip file and store the content in 
+# the "Samsung_Data" directory.
+
+#path_to_zip_file <- paste(working_directory, 
+ #                         '/UCI_Samsung_Galaxy_S_data.zip',
+  #                        sep = '')
+
+#unzip(zipfile = path_to_zip_file,
+ #     overwrite = T,
+  #    exdir = "Samsung_Data")
+
+
+
+# Question 1: Access the training and test 'X' datasets, 
+# the training and test 'y' datasets, and merge the datasets. 
+
+
+# Extract the training and test 'X' and 'y' datasets and merge them together.
+X_train <- read.table('./Samsung_Data/UCI HAR Dataset/train/X_train.txt', quote= '')
+X_test <- read.table('./Samsung_Data/UCI HAR Dataset/test/X_test.txt', quote= '')
 X_merged <- rbind(X_train,X_test)
 
-Y_train <- read.table('./Data_Project/UCI HAR Dataset/train/y_train.txt', quote='')
-Y_test <- read.table('./Data_Project/UCI HAR Dataset/test/y_test.txt', quote='')
-Y_merged <- rbind(Y_train, Y_test)
+y_train <- read.table('./Samsung_Data/UCI HAR Dataset/train/y_train.txt', quote='')
+y_test <- read.table('./Samsung_Data/UCI HAR Dataset/test/y_test.txt', quote='')
+y_merged <- rbind(y_train, y_test)
 
-DF <- cbind(Y_merged, X_merged)
+DF <- cbind(y_merged, X_merged)
 
 
 
-# Question 4 and 2. Appropriately label the data set 
+# Question 4 and 2. 
+
+# Appropriately label the data set 
 # with descriptive variable names.
-# Extract the features corresponding to "mean" and "std" measurements. 
+
+# Extract the features corresponding to "mean" or
+# "std" (standard deviation) measurements. 
 
 
 # Rename the variables of the 'DF' dataset.
-features <- read.table('./Data_Project/UCI HAR Dataset/features.txt',
+features <- read.table('./Samsung_Data/UCI HAR Dataset/features.txt',
                        quote='')
 
 X_variable_names <- as.character(features[[2]])
@@ -52,7 +108,7 @@ DF <- DF[c('ACTIVITY', X_mean_and_std_variables)]
 # Question 3: Use descriptive activity names 
 # to name the activities in the 'ACTIVITY' column of the dataset 'DF'.
 
-activity_labels <- read.table('./Data_Project/UCI HAR Dataset/activity_labels.txt', quote='')
+activity_labels <- read.table('./Samsung_Data/UCI HAR Dataset/activity_labels.txt', quote='')
 colnames(activity_labels) <- 'ACTIVITY'
 
 DF[1] <- merge(activity_labels, DF[1], 'ACTIVITY')[2]
@@ -64,8 +120,8 @@ DF <- transform(DF,ACTIVITY = tolower(ACTIVITY))
 # with the average of each variable for each activity and each subject.
 
 # Get and merge the "subject" datasets.
-subject_train <- read.table('./Data_Project/UCI HAR Dataset/train/subject_train.txt', quote='')
-subject_test <- read.table('./Data_Project/UCI HAR Dataset/test/subject_test.txt', quote='')
+subject_train <- read.table('./Samsung_Data/UCI HAR Dataset/train/subject_train.txt', quote='')
+subject_test <- read.table('./Samsung_Data/UCI HAR Dataset/test/subject_test.txt', quote='')
 subject_merged <- rbind(subject_train, subject_test)
 colnames(subject_merged) <- 'SUBJECT'
 
